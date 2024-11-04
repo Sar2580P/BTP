445c5a372273033b374f4231f6ffb23612723199
Sar2580P <sporwal1818@gmail.com>
2024-11-04 10:15:06 +0530

    re-structured code

--- modelling/dataloaders.py
+++ modelling/dataloaders.py
@@ -14,5 +14,5 @@ sda_test_dataset = SDA_Dataset(pd.read_csv(f"{BASE_DIR}/Full_Test_Set.csv"))
 
 sda_tr_loader = DataLoader(sda_tr_dataset, batch_size=sda_config['BATCH_SIZE'], shuffle=True, num_workers=4)
 sda_val_loader = DataLoader(sda_val_dataset, batch_size=sda_config['BATCH_SIZE'], shuffle=False, num_workers=4)
-sda_test_dataset = DataLoader(sda_test_dataset, batch_size=sda_config['BATCH_SIZE'], shuffle=False, num_workers=4)
+sda_test_loader = DataLoader(sda_test_dataset, batch_size=sda_config['BATCH_SIZE'], shuffle=False, num_workers=4)
 # __________________________________________________________________________________________________________________________
