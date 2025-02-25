281eba0a6b785e0ae9c60b04496c76daf371d9ff
Sar2580P <sporwal1818@gmail.com>
2025-02-26 04:20:56 +0530

    added lr_monitor callback

--- modelling/callbacks.py
+++ modelling/callbacks.py
@@ -1,27 +1,31 @@
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks import ModelCheckpoint, RichProgressBar, RichModelSummary
 from  pytorch_lightning.callbacks.progress.rich_progress import RichProgressBarTheme
-from utils import read_yaml
+from pytorch_lightning.callbacks import LearningRateMonitor
 
-callback_configs = read_yaml('modelling/stage_classifier/config.yaml')['callbacks']
+def get_callbacks(callback_config):
+   early_stop_callback = EarlyStopping(
+      monitor=callback_config['EarlyStopping']['monitor'],
+      min_delta=callback_config['EarlyStopping']['min_delta'],
+      patience=callback_config['EarlyStopping']['patience'],
+      verbose=True,
+      mode=callback_config['EarlyStopping']['mode'],
+   )
 
-early_stop_callback = EarlyStopping(
-   monitor=callback_configs['EarlyStopping']['monitor'],
-   min_delta=callback_configs['EarlyStopping']['min_delta'],
-   patience=callback_configs['EarlyStopping']['patience'],
-   verbose=True,
-   mode=callback_configs['EarlyStopping']['mode'],
-)
+   theme = RichProgressBarTheme(metrics='green', time='yellow', progress_bar_finished='#8c53e0' ,
+                                progress_bar='#c99e38')
+   rich_progress_bar = RichProgressBar(theme=theme)
 
-theme = RichProgressBarTheme(metrics='green', time='yellow', progress_bar_finished='#8c53e0' ,progress_bar='#c99e38')
-rich_progress_bar = RichProgressBar(theme=theme)
+   rich_model_summary = RichModelSummary(max_depth=3)
 
-rich_model_summary = RichModelSummary(max_depth=3)
+   checkpoint_callback = ModelCheckpoint(
+      monitor=callback_config['ModelCheckpoint']['monitor'],
+      save_top_k=callback_config['ModelCheckpoint']['save_top_k'],
+      mode=callback_config['ModelCheckpoint']['mode'],
+      save_last=callback_config['ModelCheckpoint']['save_last'],
+      verbose=True,
+   )
+   lr_monitor = LearningRateMonitor(logging_interval='step')
 
-checkpoint_callback = ModelCheckpoint(
-    monitor=callback_configs['ModelCheckpoint']['monitor'],
-    save_top_k=callback_configs['ModelCheckpoint']['save_top_k'],
-    mode=callback_configs['ModelCheckpoint']['mode'],
-    save_last=callback_configs['ModelCheckpoint']['save_last'],
-    verbose=True,
- )
+   return (early_stop_callback, checkpoint_callback, 
+           rich_progress_bar, rich_model_summary, lr_monitor)
